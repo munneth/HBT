ac5e5964f1eb4e9b5749c0e837430fdef8435826
munneth <mugill@ucdavis.edu>
2025-08-25 14:41:40 -0700

    Add client-side rendering and refactor 3D model loading in Scene component

--- hbt/src/components/scene.tsx
+++ hbt/src/components/scene.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 // src/Scene.tsx
 import {
   Circle,
@@ -7,7 +9,7 @@ import {
   useProgress,
 } from "@react-three/drei";
 import { Canvas, useLoader } from "@react-three/fiber";
-import React, { Suspense } from "react";
+import React, { Suspense, useState, useEffect } from "react";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 function Loader() {
@@ -16,29 +18,56 @@ function Loader() {
   return <Html center>{progress} % loaded</Html>;
 }
 
-const Scene: React.FC = () => {
-  try {
-    const gltf = useLoader(GLTFLoader, "/models/peterbilt_379.glb");
+function Model() {
+  const gltf = useLoader(GLTFLoader, "/models/peterbilt_379.glb");
+  return <primitive object={gltf.scene} position={[0, 0, 0]} />;
+}
 
-    return (
-      <Suspense fallback={<div>Loading 3D Model...</div>}>
+const Scene: React.FC = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return <div>Loading 3D Model...</div>;
+  }
+
+  return (
+    <div style={{ width: "100%", height: "700px" }}>
+      <Suspense
+        fallback={
+          <div
+            style={{
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            Loading 3D Model...
+          </div>
+        }
+      >
         <Canvas
           camera={{ position: [0, 5, 50] }}
-          style={{ width: "100%", height: "700px" }}
+          style={{ width: "100%", height: "100%" }}
         >
           <directionalLight
             position={[-1.3, 6.0, 4.4]}
             castShadow
             intensity={Math.PI * 1}
           />
-          <primitive object={gltf.scene} position={[0, 0, 0]} />
+          <Suspense fallback={<Loader />}>
+            <Model />
+          </Suspense>
           <OrbitControls target={[0, 1, 0]} enableZoom={false} />
         </Canvas>
       </Suspense>
-    );
-  } catch (error) {
-    return <div>Loading 3D Model...</div>;
-  }
+    </div>
+  );
 };
 
 export default Scene;
